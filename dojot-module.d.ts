1ffccaf26709eba87a2d5c02ada16fa63ca39ca8
Giovanni Curiel dos Santos <giovannicuriel@gmail.com>
2018-08-19 15:33:39 -0300

    refactor: moving things around

---
+++ dojot-module.d.ts
@@ -0,0 +1,4 @@
+declare module "dojot-module" {
+
+class
+};
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = {
-    Logger: require('./libs/Log/logger'),
-    KafkaConsumer: require('./libs/Kafka/Consumer'),
-    KafkaProducer: require('./libs/Kafka/Producer')
-}
-
+    Logger: require('./lib/log/logger'),
+    KafkaConsumer: require('./lib/kafka/Consumer'),
+    KafkaProducer: require('./lib/kafka/Producer'),
+    Messenger: require("./lib/messenger").Messenger,
+};
