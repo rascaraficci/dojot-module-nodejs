a9b10283fadc533966d73cd3e76bd517818b0314
Giovanni Curiel dos Santos <giovannicuriel@gmail.com>
2018-08-22 14:19:56 -0300

    feat: getting tenants before anything else

--- index.js
+++ index.js
@@ -3,4 +3,6 @@ module.exports = {
     KafkaConsumer: require('./lib/kafka/Consumer'),
     KafkaProducer: require('./lib/kafka/Producer'),
     Messenger: require("./lib/messenger").Messenger,
+    Config: require("./lib/config"),
+    Auth: require("./lib/auth"),
 };
